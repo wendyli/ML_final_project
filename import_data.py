936c7ccf53bae028fb36e26df7e43d4601a943ef
isuarezr <isuarezr@stanford.edu>
2017-11-03 19:29:08 -0700

    Extracts data parsing into a function

--- import_data.py
+++ import_data.py
@@ -2,29 +2,34 @@ import csv
 
 file_data_name = "data_Q1_2017/2017-01-01.csv"
 
-with open(file_data_name, 'r') as file_data:
-	reader = csv.reader(file_data, delimiter='\t')
-	data_list = list(reader)
 
-	actual_data_list = data_list[1:]
-	attributes_list = data_list[0][0].split(',')
+# Returns tuple consisting of the following:
+# - the set of attribute names
+# - a map from a HDD serial number to its data vector, where each data vector
+#   is a map from an attribute name to its corresponding integer value
+def extractData(filename):
+    with open(filename, 'r') as file_data:
+        reader = csv.reader(file_data, delimiter=',')
+        data_list = list(reader)
 
-	print(attributes_list)
+        actual_data_list = data_list[1:]
+        attributes_list = data_list[0]
 
+        serial_num_to_data_vector = {}
 
-	hd_to_feature = {}
+        num_features = len(attributes_list)    
+        num_examples = len(actual_data_list) 
+        for i in range(0, num_examples):
+            ith_raw_data = actual_data_list[i]
 
-	num_features = len(attributes_list)    
-	num_examples = len(actual_data_list) 
-	for i in range(0, num_examples):
-		hard_drive_data = actual_data_list[i][0].split(',')
+            ith_data_vector = {}
+            
+            # First 3 are data, serial number, and model, which are irrelevant
+            for j in range(3, num_features):
 
-		feature_to_data = {}
-		for j in range(3, num_features):
+                if (ith_raw_data[j] != ''):
+                    ith_data_vector[attributes_list[j]] = int(ith_raw_data[j])
 
-			if (hard_drive_data[j] != ''):
-				feature_to_data[attributes_list[j]] = hard_drive_data[j]
-
-		hd_to_feature[hard_drive_data[1]] = feature_to_data 
-
-		print hd_to_feature
+            serial_num_to_data_vector[ith_raw_data[1]] = ith_data_vector
+        
+        return (set(attributes_list), serial_num_to_data_vector)
