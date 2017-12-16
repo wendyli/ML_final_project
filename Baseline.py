720ea47a7b4c668702cef03b19ba0537024e7471
Wendy <liwy52395@gmail.com>
2017-12-15 17:49:25 -0800

    modified for new Q2 data

--- Baseline.py
+++ Baseline.py
@@ -1,51 +1,23 @@
 print(__doc__)
-
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import cycle
 from sklearn.metrics import roc_curve, auc
-
-
 import csv
 import json
 
-'''
-Only for Baseline analysis of Q1 2016  
+total_failure = 0
 
-'''
+HDD_dict = {}  # key = serial num, value = vector of 5 raw values and failure value
+days = [31,29, 31, 30, 31, 30] #1,2,3
+for m in xrange(4,7):	
+	for d in range(1,days[m-1] + 1):
 
-'''
-Dict format:
-
-Key: serial number of the hard disk drive 
-
-Dict:
-0 - 'predicted_status' 0 = not failed, 1 = failed
-1 - 'predicted_fail_date' -> if no fail predicted, value is None
-2 - 'actual_status' 0 = not failed, 1 = failed
-3 - 'actual_fail_date' -> if not failed, value is None
-4 - 'smart_5_raw'  		i = 14
-5 - 'smart_187_raw'		i = 38
-6 - 'smart_188_raw'		i = 40
-7 - 'smart_197_raw'		i = 58
-8 - 'smart_198_raw'		i = 60
-
-'''
-
-		
-SMART_dict = {}  # key = serial num
-
-days = [31, 29, 31] #1,2,3
-
-for m in xrange(1,4):
-
-	day = days[m-1]	
-	for i in range(1,day+1):
-
-		name_i = '0' + str(i) if i < 10 else str(i)
-		file_data_name = "../data_2016_Q1/2016-0{0}-{1}.csv".format(m, name_i)
+		name_i = '0' + str(d) if d < 10 else str(d)
+		file_data_name = "../test_data/2016-0{0}-{1}_smoothed_0.csv".format(m, name_i)
 		print file_data_name
-
+		
+		# open each file and read specific attributes
 		with open(file_data_name, 'r') as file_data:
 			reader = csv.reader(file_data, delimiter='\t')
 			data_list = list(reader)
@@ -53,46 +25,34 @@ for m in xrange(1,4):
 			actual_data_list = data_list[1:] # contains all other data after first row
 			attributes_list = data_list[0][0].split(',') # contains the headers and attribute names 
 			
-			attribute_size = len(attributes_list) # total number of attributes
-			feature_count = len(actual_data_list) # total number of SMART features
-
 			# check index values for all attributes we want
 			important_attributes = [ 'smart_5_raw', 'smart_187_raw', 'smart_188_raw', 'smart_197_raw', 'smart_198_raw' ]
 
 			for i, value in enumerate(actual_data_list):
 				
 				hard_drive_data = actual_data_list[i][0].split(',') # individual hard drive's data
-				
 				serial_number = hard_drive_data[1]
 
 				attribute_dict = {}
-				if serial_number in SMART_dict:
-					attribute_dict = SMART_dict[serial_number]
+				# attribute dict contains
+				# 'raw values', 'failed'
+				if serial_number in HDD_dict:
+					attribute_dict = HDD_dict[serial_number]
 
-			
-				attribute_dict['actual_status'] = int(hard_drive_data[4])
-				#print attributes_list[4], ':', attribute_dict['actual_status']
-				
-				attribute_dict['date'] = hard_drive_data[0]
-				if attribute_dict['actual_status'] == 1:
-					actual_fail_date = attribute_dict['date'] ### MODIFY ONCE GOING THROUGH MULTIPLE FILES
+				# set failure status
+				if 'failed' not in attribute_dict:
+					attribute_dict['failed'] = int(hard_drive_data[4])
 				else: 
-					actual_fail_date = None
+					if attribute_dict['failed'] != 1:
+						attribute_dict['failed'] = int(hard_drive_data[4])
 
-				attribute_dict['actual_fail_date'] = actual_fail_date
-				#print 'actual_fail_date:', actual_fail_date
+				total_failure += int(hard_drive_data[4])
 
-				
 				attribute_dict['smart_5_raw'] = int(hard_drive_data[14]) if hard_drive_data[14] != '' else None
-				#print attributes_list[14], ':', hard_drive_data[14]
 				attribute_dict['smart_187_raw'] = int(hard_drive_data[38]) if hard_drive_data[38] != '' else None
-				#print attributes_list[38], ':', hard_drive_data[38]
 				attribute_dict['smart_188_raw'] = int(hard_drive_data[40]) if hard_drive_data[40] != '' else None
-				#print attributes_list[40], ':', hard_drive_data[40]
 				attribute_dict['smart_197_raw'] = int(hard_drive_data[58]) if hard_drive_data[58] != '' else None
-				#print attributes_list[58], ':', hard_drive_data[58]
 				attribute_dict['smart_198_raw'] = int(hard_drive_data[60]) if hard_drive_data[60] != '' else None
-				#print attributes_list[60], ':', hard_drive_data[60]
 
 				# Get total value of all the attributes
 				total = 0
@@ -102,55 +62,43 @@ for m in xrange(1,4):
 				total += attribute_dict['smart_197_raw'] if attribute_dict['smart_197_raw'] != None else 0
 				total += attribute_dict['smart_198_raw'] if attribute_dict['smart_198_raw'] != None else 0
 
-				#check if we have already predicted failure for this hard drive 
-				if 'status_predicted' not in attribute_dict:
-					attribute_dict['status_predicted'] = 0
+				# check if we have already predicted failure for this hard drive 
+				if 'prediction' not in attribute_dict:
+					attribute_dict['prediction'] = 0
 
-				already_predicted_failture = attribute_dict['status_predicted'] == 1
+				already_predicted_failture = attribute_dict['prediction'] == 1
 
 				if (not already_predicted_failture):
 					if total > 0:
-						attribute_dict['status_predicted'] = 1
-						attribute_dict['predicted_fail_date'] = attribute_dict['date'] #Change 
+						attribute_dict['prediction'] = 1
 					else:
-						attribute_dict['status_predicted'] = 0
-						attribute_dict['predicted_fail_date'] = None
+						attribute_dict['prediction'] = 0
 
-				SMART_dict[serial_number] = attribute_dict
+				HDD_dict[serial_number] = attribute_dict
 
 # Get information from each HDD
-
-count_actual_failures = 0
-count_actual_healthy = 0
-count_predicted_failures = 0
 
 false_pos = 0 
 false_neg = 0
 true_pos = 0
 true_neg = 0
 
-num_examples = len(SMART_dict)
-
 actual =[]
 predictions = []
 
-for key in SMART_dict:
-	curr_attribute_dict = SMART_dict[key]
-	count_actual_failures += curr_attribute_dict['actual_status']
-	count_actual_healthy += (int)(curr_attribute_dict['actual_status'] == 0)
-	count_predicted_failures += curr_attribute_dict['status_predicted']
-	actual.append(curr_attribute_dict['actual_status'])
-	predictions.append(curr_attribute_dict['status_predicted'])
+for key in HDD_dict:
+	disk = HDD_dict[key]
+	actual.append(disk['failed'])
+	predictions.append(disk['prediction'])
 
-	if curr_attribute_dict['status_predicted'] == 1 and curr_attribute_dict['actual_status'] == 0:
-		false_pos += 1
-	elif curr_attribute_dict['status_predicted'] == 0 and curr_attribute_dict['actual_status'] == 1:
-		false_neg += 1
-	elif curr_attribute_dict['status_predicted'] == 0 and curr_attribute_dict['actual_status'] == 0:
+	if disk['prediction'] == 1 and disk['failed'] == 0:
+		false_pos +=1
+	if disk['prediction'] == 0 and disk['failed'] == 1:
+		false_neg +=1
+	if disk['prediction'] == 0 and disk['failed'] == 0:
 		true_neg += 1
-	elif curr_attribute_dict['status_predicted'] == 1 and curr_attribute_dict['actual_status'] == 1:
+	if disk['prediction'] == 1 and disk['failed'] == 1:
 		true_pos += 1
-
 
 PPV = float(true_pos)/ (true_pos + false_pos)
 NPV = float(true_neg)/ (false_neg + true_neg)
@@ -158,10 +106,6 @@ Accuracy= (float(true_pos) + true_neg)/ (true_pos + false_pos+ false_neg + true_
 Sensitivity = float(true_neg)/(false_pos + true_neg)
 Specificity = float(true_pos)/(true_pos + false_neg)
 
-print 'True failures:', count_actual_failures
-print 'True healthy:', count_actual_healthy
-print 'Predicted failures:', count_predicted_failures
-print 'Total HDD count:', len(SMART_dict)
 print '--- RAW values ----'
 print "False positive: {0}".format(false_pos)
 print "True positive:{0}".format(true_pos)
@@ -176,13 +120,6 @@ print 'Specificity', Specificity
 print '------ ROC ------'
 print 'Precision', float(true_pos)/(true_pos + false_pos)
 print 'Recall', float(true_pos)/(true_pos+ false_neg)
-
-
-
-#REPLACE: actual values
-#actual = [1,1,1,0,0,0] 
-# REPLACE: actual predictions 
-#predictions = [0.9,0.9,0.9,0.1,0.1,0.1]
 
 false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predictions)
 roc_auc = auc(false_positive_rate, true_positive_rate)
